44a0c60912f6dfdda81febbf9b86ee36f57f759d
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2023-12-03 01:38:21 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+//mods added
+//Implement the speed increase that happens after 30 seconds in the original game (1)
+//Implement mouse control for player movement and mouse click to fire (5)
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits [Hint: use this.timer.addEvent()] (5)
+//Implement an alternating two-player mode (5)
+//Create 4 new explosion sound effects and randomize which one plays on impact (3)
+//Display the time remaining (in seconds) on the screen (3)
+//Implement the 'FIRE' UI text from the original game (1)
 //console.log("hello world");
 let config = {
     type: Phaser.AUTO,
