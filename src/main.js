2f18341f6410334907839526d393b9b109397414
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2023-12-03 01:40:20 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 //Jack O'Brien
-//mods added
-//Implement the speed increase that happens after 30 seconds in the original game (1)
-//Implement mouse control for player movement and mouse click to fire (5)
-//Implement a new timing/scoring mechanism that adds time to the clock for successful hits [Hint: use this.timer.addEvent()] (5)
-//Implement an alternating two-player mode (5)
-//Create 4 new explosion sound effects and randomize which one plays on impact (3)
-//Display the time remaining (in seconds) on the screen (3)
-//Implement the 'FIRE' UI text from the original game (1)
+//Rocket Patrol New Age
+//20-30 hours
+//increased ship speed after 30 seconds(1)
+//full mouse control when playing(5)
+//time increased by 1 each time ship taken out(5)
+//2 player alternation(5)
+//4 random sounds for explosion credited in comments above where implemented(3)
+//timer on the right counting down(3)
+//fire from original game(1)
+//sound credit
+//https://freesound.org/people/Prof.Mudkip/sounds/386862/
+//https://freesound.org/people/InspectorJ/sounds/448226/
+//https://freesound.org/people/n_audioman/sounds/320366/
+ //https://freesound.org/people/n_audioman/sounds/276342/
+//
+//code credit:
+//https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser
+//https://www.html5gamedevs.com/topic/37506-pick-random-element/
+//might have forgotten some, but these are the main ones
 //console.log("hello world");
 let config = {
     type: Phaser.AUTO,
